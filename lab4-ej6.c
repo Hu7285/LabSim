1d0b90a8c63650493d29d8445d7ec616da610d08
Hu7285 <61610351+Hu7285@users.noreply.github.com>
2020-03-31 02:00:59 -0600

    Add files via upload

--- lab4-ej6.c
+++ lab4-ej6.c
@@ -15,84 +15,84 @@ Salidas: r
 //numerar los pasos del pseudocodigo
 
 /*Declaracion de variables.
-sa(k), sb(k), sc(k), sc(k): double, funciones recursivas, apoyaran para el calculo de las sumatorias.
+sa(n), sb(n), sc(n), sc(n): double, funciones recursivas, apoyaran para el calculo de las sumatorias.
 k: double, numero ingresado por usuario que determinara el limite de la sumatoria.
 */
-double sa(double k);
-double sb(double k);
-double sc(double k);
-double sd(double k);
+double sa(double n);
+double sb(double n);
+double sc(double n);
+double sd(double n);
 //Funcion principal.
 int main() {
     //Declarando k de forma local.
-    double k;
-    //Pidiendo al usuario un numero y guardandolo a k.
+    double n;
+    //Pidiendo al usuario un numero y guardandolo a n.
     printf("Please enter a value: ");
-    scanf("%lf", &k);
+    scanf("%lf", &n);
     //Desplegando los resultados de las sumatorias.
-    printf("The sum a, k^2(k-3), from [1,%.0lf] is: \n %lf \n", k, sa(k));
-    printf("The sum b, 3/(k-1), from [2,%.0lf] is: \n %lf \n", k, sb(k));
-    printf("The sum c, 5^(-1/2)((1+5^(1/2))/2)^k-5^(-1/2)((1-5^(1/2))/2), from [1,%.0lf] is: \n %lf \n", k, sc(k));
-    printf("The sum d, 0.1(3*2^(k-2)+4), from [2,%.0lf] is: \n %lf \n", k, sd(k));
+    printf("The sum a, k^2(k-3), from [1,%.0lf] is: \n %lf \n", n, sa(n));
+    printf("The sum b, 3/(k-1), from [2,%.0lf] is: \n %lf \n", n, sb(n));
+    printf("The sum c, 5^(-1/2)((1+5^(1/2))/2)^k-5^(-1/2)((1-5^(1/2))/2), from [1,%.0lf] is: \n %lf \n", n, sc(n));
+    printf("The sum d, 0.1(3*2^(k-2)+4), from [2,%.0lf] is: \n %lf \n", n, sd(n));
 return 0;
 }
 
-//Funcion recursiva sa(k).
-double sa(double k) {
-    //Funcion recursiva que llegara al valor de la sumatoria cuando k=1 y regresara el valor de la sumatoria.
-    /*if: verifica k>1.
+//Funcion recursiva sa(n).
+double sa(double n) {
+    //Funcion recursiva que llegara al valor de la sumatoria cuando n=1 y regresara el valor de la sumatoria.
+    /*if: verifica n>1.
     Verdad: hace la llamada recursiva y regresa la formula de la sumatoria con sa(k-1), con el valor anterior.
-    Falso: regresa el valor de la sumatoria cuando k=1.
+    Falso: regresa el valor de la sumatoria cuando n=1.
     */
-    if ( k>1 ) {
-        return sa(k-1)+pow(k,2)*(k-3);
+    if ( n>1 ) {
+        return sa(n-1)+pow(n,2)*(n-3);
     }
     else {
         return pow(1,2)*(1-3);
     }
 }
 
-//Funcion recursiva sb(k).
-double sb(double k) {
-    //NOTA: esta sumatoria tiene que el caso k=1 es un valor inexistente, por lo tanto la sumatoria debe de empezar en k=2.
-    //Funcion recursiva que llegara al valor de la sumatoria cuando k=2 y regresara el valor de la sumatoria.
-    /*if: verifica k>2.
+//Funcion recursiva sb(n).
+double sb(double n) {
+    //NOTA: esta sumatoria tiene que el caso n=1 es un valor inexistente, por lo tanto la sumatoria debe de empezar en n=2.
+    //Funcion recursiva que llegara al valor de la sumatoria cuando n=2 y regresara el valor de la sumatoria.
+    /*if: verifica n>2.
     Verdad: hace la llamada recursiva y regresa la formula de la sumatoria con sa(k-1), con el valor anterior.
-    Falso: regresa el valor de la sumatoria cuando k=2.
+    Falso: regresa el valor de la sumatoria cuando n=2.
     */
-    if ( k>2 ) {
-        return sb(k-1)+(3/(k-1));
+    if ( n>2 ) {
+        return sb(n-1)+(3/(n-1));
     }
     else {
         return (3)/(2-1);
     }
 }
 
-//Funcion recursiva sc(k).
-double sc(double k) {
-    //Funcion recursiva que llegara al valor de la sumatoria cuando k=1 y regresara el valor de la sumatoria.
-    /*if: verifica k>1.
-    Verdad: hace la llamada recursiva y regresa la formula de la sumatoria con sa(k-1), con el valor anterior.
+//Funcion recursiva sc(n).
+double sc(double n) {
+    //Funcion recursiva que llegara al valor de la sumatoria cuando n=1 y regresara el valor de la sumatoria.
+    /*if: verifica n>1.
+    Verdad: hace la llamada recursiva y regresa la formula de la sumatoria con sa(n-1), con el valor anterior.
     Falso: regresa el valor de la sumatoria cuando k=1.
     */
-    if ( k>1 ) {
-        return sc(k-1)+(1/sqrt(5))*(pow((1+sqrt(5))/2,k))-(1/sqrt(5))*(pow((1-sqrt(5))/2,k));
+    if ( n>1 ) {
+        return sc(n-1)+(1/sqrt(5))*(pow((1+sqrt(5))/2,n))-(1/sqrt(5))*(pow((1-sqrt(5))/2,n));
     }
     else {
         return (1/sqrt(5))*(pow((1+sqrt(5))/2,1))-(1/sqrt(5))*(pow((1-sqrt(5))/2,1));
     }
 }
 
-//Funcion recursiva sd(k).
-double sd(double k) {
-    //NOTA: esta sumatoria tiene que el caso k=1 es un valor inexistente, por lo tanto la sumatoria debe de empezar en k=2.
-    //Funcion recursiva que llegara al valor de la sumatoria cuando k=2 y regresara el valor de la sumatoria.
-    /*if: verifica k>2.
+//Funcion recursiva sd(n).
+double sd(double n) {
+    //NOTA: esta sumatoria tiene que el caso k=1 es un valor inexistente, por lo tanto la sumatoria debe de empezar en n=2.
+    //Funcion recursiva que llegara al valor de la sumatoria cuando n=2 y regresara el valor de la sumatoria.
+    /*if: verifica dnk>2.
     Verdad: hace la llamada recursiva y regresa la formula de la sumatoria con sa(k-1), con el valor anterior.
     Falso: regresa el valor de la sumatoria cuando k=2.
     */
-    if ( k>2 ) {
-        return sd(k-1)+(0.1)*(3*pow(2,k-2)+4);
+    if ( n>2 ) {
+        return sd(n-1)+(0.1)*(3*pow(2,n-2)+4);
     }
     else {
         return (0.1)*(3*pow(2,2-2)+4);
